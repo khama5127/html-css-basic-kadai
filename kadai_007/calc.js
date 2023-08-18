6c3e15ef6ad097b510b4a0fa20e4c8bdf86484c6
khama5127 <khama5127@gmail.com>
2023-08-18 17:41:17 +0900

    v1

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,13 +1,13 @@
 
-let num = 3;
+let num = 10;
 
-if ( num / 15){
+if ( 0 === num % 15){
     console.log('3と5の倍数です');
 }
-else if ( num / 3){
+else if ( 0 === num % 3){
     console.log('3の倍数です');
 }
-else if ( num / 5){
+else if ( 0 === num % 5){
     console.log('5の倍数です');
 }
 else{
